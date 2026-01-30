91610b43fc12eef88e27ff41e9f5a8dbff541b37
Saheli-M01 <sahelimondal7845@gmail.com>
2026-01-30 15:30:57 +0530

    Practice

---
+++ 01basicreact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai";
+
+function App() {
+  const userName = "chaman";
+
+  return (
+    <>
+      <h2>Kaanp kahe rahe ho???</h2>
+      <Chai />
+      {userName}
+    </>
+  );
+}
+
+export default App;
