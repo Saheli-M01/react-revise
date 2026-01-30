3db5579671a3c1086720a0edbc847f558e538d5b
Saheli-M01 <sahelimondal7845@gmail.com>
2026-01-30 17:42:59 +0530

    Added bg-changer project

--- 04bg_changer/src/App.jsx
+++ 04bg_changer/src/App.jsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState("olive");
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="w-full h-screen duration-200 border"
+        style={{ backgroundColor: color }}
+      >
+        <div className="flex fixed flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-2xl bg-white rounded-full px-3 py-2">
+            <button
+              onClick={() => setColor("red")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-xl"
+              style={{ backgroundColor: "red" }}
+            >
+              Red
+            </button>
+            <button
+             onClick={() => setColor("green")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-xl"
+              style={{ backgroundColor: "green" }}
+            >
+              Green
+            </button>
+            <button
+             onClick={() => setColor("blue")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-xl"
+              style={{ backgroundColor: "blue" }}
+            >
+              Blue
+            </button>
+            <button
+             onClick={() => setColor("brown")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-xl"
+              style={{ backgroundColor: "brown" }}
+            >
+              Brown
+            </button>
+            <button
+             onClick={() => setColor("hotpink")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-xl"
+              style={{ backgroundColor: "hotpink" }}
+            >
+              Hot Pink
+            </button>{" "}
+            <button
+             onClick={() => setColor("teal")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-xl"
+              style={{ backgroundColor: "teal" }}
+            >
+              Teal
+            </button>{" "}
+            <button
+             onClick={() => setColor("violet")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-xl"
+              style={{ backgroundColor: "violet" }}
+            >
+              Violet
+            </button>{" "}
+            <button
+             onClick={() => setColor("lime")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-xl"
+              style={{ backgroundColor: "lime" }}
+            >
+              Lime
+            </button>{" "}
+            <button
+             onClick={() => setColor("fuchsia")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-xl"
+              style={{ backgroundColor: "fuchsia" }}
+            >
+              Fuchsia
+            </button>{" "}
+            <button
+             onClick={() => setColor("indigo")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-xl"
+              style={{ backgroundColor: "indigo" }}
+            >
+              Indigo
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
