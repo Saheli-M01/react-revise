3eaff11b802af5de2a8bf53690c429eec3b06125
Saheli-M01 <sahelimondal7845@gmail.com>
2026-01-30 16:05:49 +0530

    Counter

---
+++ 02project/test_project/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function App() {
+  let [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+    if (counter <= 19) {
+      setCounter(counter + 1);
+    }
+  };
+  const subtractValue = () => {
+    if (counter >= 1) {
+      setCounter(counter - 1);
+    }
+  };
+  return (
+    <>
+      <h1>Bas aise hi</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value: {counter}</button>
+      <br />
+      <button onClick={subtractValue}>Remove Value: {counter}</button>
+    </>
+  );
+}
+
+export default App;
